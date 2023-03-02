ce753a40ea8bd92fd65362c4828391a76b09d742
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-02 16:11:02 +0100

    [IT 1] - app skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import countries from "./countries.json";
+
+import Layout from './components/Layout';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/countries" element={<CountriesList />}>
+            <Route path=":code" element={<CountryDetails />} />
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }

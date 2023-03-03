03a04769a535a71166a36dcfb4cd51b8afb1b867
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-03 12:09:42 +0100

    [IT 4] - fetch data through api

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import countries from "./countries.json";
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://ih-countries-api.herokuapp.com/countries');
+        const json = await response.json();
+        console.log(json);
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path='/' element={<Navigate to="/countries" />} />
-        <Route path="/countries" element={<CountriesList countries={countries} />}>
-          <Route path=":code" element={<CountryDetails countries={countries} />} />
+        <Route path="/countries" element={<CountriesList countries={data} />}>
+          <Route path=":code" element={<CountryDetails countries={data} />} />
         </Route>
         <Route path='*' element={<h2>Error 404</h2>}></Route>
       </Routes>
